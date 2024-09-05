aa15861fceba5d84b661e71fa74b7eeb286e9320
Rahat Baksh <rahat392@gmail.com>
2024-09-05 17:10:47 +0600

    checking for empty emdia objects.

--- src/Http/Resources/ShopCategoryResource.php
+++ src/Http/Resources/ShopCategoryResource.php
@@ -9,12 +9,17 @@ class ShopCategoryResource extends JsonResource
     public function toArray($request)
     {
         $medias = $this->getMedia('category_image');
-
+        $mediaUrl = null;
+    
+        if (!empty($medias) && isset($medias[0]) && method_exists($medias[0], 'getFullUrl')) {
+            $mediaUrl = $medias[0]->getFullUrl();
+        }
+    
         return [
             'name' => $this->name,
             'slug' => $this->slug,
             'description' => $this->description,
-            'media' => $medias[0]->getFullUrl(),
+            'media' => $mediaUrl,
         ];
     }
 }
