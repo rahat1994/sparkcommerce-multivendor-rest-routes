581efc5e68665ade83bb122ec984e33cee23bb56
rahat1994 <rahat1994@users.noreply.github.com>
2024-09-06 10:15:06 +0000

    Fix styling

--- src/Http/Controllers/VendorController.php
+++ src/Http/Controllers/VendorController.php
@@ -4,19 +4,18 @@ namespace Rahat1994\SparkcommerceMultivendorRestRoutes\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Rahat1994\SparkcommerceMultivendor\Models\SCMVVendor;
 use Rahat1994\SparkcommerceMultivendorRestRoutes\Http\Resources\SCMVVendorResource;
 use Rahat1994\SparkcommerceRestRoutes\Http\Resources\SCCategoryResource;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class VendorController extends Controller
 {
     public function topVendors(Request $request)
     {
         $topVendors = SCMVVendor::whereJsonContains('meta->is_top_vendor', 1)
-        ->with('media', 'sCProducts')
-        ->get();
+            ->with('media', 'sCProducts')
+            ->get();
+
         return SCMVVendorResource::collection($topVendors);
     }
 
