5c82c5c1153e0dedb9ae09692ba4e89aec501044
Rahat Baksh <rahat392@gmail.com>
2024-09-10 21:13:35 +0600

    Modifying CartController and adding a new Exception

---
+++ src/Exceptions/VendorNotSameException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rahat1994\SparkcommerceMultivendorRestRoutes\Exceptions;
+
+use Exception;
+
+class VendorNotSameException extends Exception
+{
+    public function __construct($message = 'Vendor not same', $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
