6ffb83e54fe9da708ae71e8694a3ae8a98a665b7
rahat1994 <rahat1994@users.noreply.github.com>
2024-09-10 15:17:11 +0000

    Fix styling

--- src/Exceptions/VendorNotSameException.php
+++ src/Exceptions/VendorNotSameException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class VendorNotSameException extends Exception
 {
-    public function __construct($message = 'Vendor not same', $code = 0, Exception $previous = null)
+    public function __construct($message = 'Vendor not same', $code = 0, ?Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
