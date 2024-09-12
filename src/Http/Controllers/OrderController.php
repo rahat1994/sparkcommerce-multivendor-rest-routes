ddcd4a756b39591e953bc8b09d2fdac7e7bbb048
Rahat Baksh <rahat392@gmail.com>
2024-09-12 21:17:35 +0600

    Refactor OrderController to implement try-catch block for error handling

--- src/Http/Controllers/OrderController.php
+++ src/Http/Controllers/OrderController.php
@@ -3,15 +3,19 @@
 namespace Rahat1994\SparkcommerceMultivendorRestRoutes\Http\Controllers;
 
 use Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Rahat1994\SparkCommerce\Models\SCOrder;
 use Rahat1994\SparkcommerceMultivendorRestRoutes\Http\Resources\SCMVOrderResource;
+use Rahat1994\SparkcommerceMultivendorRestRoutes\Http\Resources\ShopCategoryResource;
 use Rahat1994\SparkcommerceRestRoutes\Http\Controllers\SCBaseController;
 
 class OrderController extends SCBaseController
 {
     public function index(Request $request)
     {
+        // TODO: [Important] Implement try catch block
         $user = Auth::guard('sanctum')->user();
 
         $params = [
@@ -21,19 +25,18 @@ class OrderController extends SCBaseController
             'order' => 'desc',
         ];
 
-        $data = $this->callHook('orderParams', $params);
+        $data = $this->callHook('orderIndexParams', $params);
 
         $params = $data ?? $params;
-
+        
         $orders = SCOrder::limit($params['limit'])
             ->where('user_id', $params['user_id'])
             ->orderBy($params['order_by'], $params['order'])
             ->paginate();
 
-        $data = $this->callHook('orderList', $orders);
+        $data = $this->callHook('afterOrderListFetch', $orders);
 
         $orders = $data ?? $orders;
-
         return SCMVOrderResource::collection($orders);
     }
 
@@ -42,19 +45,41 @@ class OrderController extends SCBaseController
         $user = Auth::guard('sanctum')->user();
 
         try {
-            $order = SCOrder::where('tracking_number', $trackingNumber)
+
+            $params = [
+                'tracking_number' => $trackingNumber,
+            ];
+    
+            $data = $this->callHook('singlerderParams', $params);
+    
+            $params = $data ?? $params;
+
+            $order = SCOrder::where('tracking_number', $params['tracking_number'])
                 ->where('user_id', $user->id)
                 ->firstOrFail();
+            
+            $data = $this->callHook('afterSingleOrderFetch', $order);
+
+            $order = $data ?? $order;
 
             return SCMVOrderResource::make($order);
-        } catch (\Throwable $th) {
+        } catch (ModelNotFoundException $th) {
             //throw $th;
             return response()->json(
                 [
-                    'message' => 'Order not found',
+                    'message' => 'resource not found',
                     'tracking_number' => $trackingNumber,
                 ],
                 404
+            );
+        }        
+        catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(
+                [
+                    'message' => 'Something went wrong',
+                ],
+                400
             );
         }
 
